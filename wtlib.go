254109353e68299016d89593e9de27ba8564330c
Marcin Szydelski <marcin@szydelscy.pl>
2021-07-01 13:33:25 +0200

    Load Config

--- wtlib.go
+++ wtlib.go
@@ -25,6 +25,19 @@ func CreateEmptyConfig(fn string) {
 	}
 }
 
+// LoadConfig loads a yaml config
+func LoadConfig(fn string) (config Config) {
+	cnfFile, err := ioutil.ReadFile(fn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(cnfFile, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
 // MkdirP creates a directory with whole path. It is an equivalent to mkdir -p
 func MkdirP(p string) (*os.File, error) {
 	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
