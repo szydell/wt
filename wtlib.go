8187258d538184c971b7958ee7f50679ea6d2a04
Marcin Szydelski <marcin@szydelscy.pl>
2021-07-01 13:19:12 +0200

    mkdirP and create empty config

---
+++ wtlib.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+// CreateEmptyConfig initializes a yaml config file
+func CreateEmptyConfig(fn string) {
+	config := Config{Record: Record{DbPath: ""}}
+	data, err := yaml.Marshal(&config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(fn, data, 0600)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// MkdirP creates a directory with whole path. It is an equivalent to mkdir -p
+func MkdirP(p string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(p+"/"), 0770); err != nil {
+		return nil, err
+	}
+	return os.Create(p)
+}
