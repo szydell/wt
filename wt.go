90c5778b04ae9b8d389dd6722132a01c31072f35
Marcin Szydelski <marcin@szydelscy.pl>
2021-07-01 13:19:31 +0200

    create an empty config when none found

--- wt.go
+++ wt.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Record struct {
+	DbPath string `yaml:"DbPath"`
+}
+
+type Config struct {
+	Record Record `yaml:"Settings"`
+}
 
 func main() {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Print("Couldn't find a home directory")
+		log.Fatal(err)
+	}
+	confFile := userHomeDir + "/.config/wt/config.yaml"
+	if _, err := os.Stat(confFile); os.IsNotExist(err) {
+		fmt.Printf("Configuration file %s not found. Creating.\n", confFile)
+		MkdirP(filepath.Dir(confFile))
+		CreateEmptyConfig(confFile)
+	}
 	fmt.Println("init")
 }
